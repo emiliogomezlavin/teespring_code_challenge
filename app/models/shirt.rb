0f5ca8d6929e160bde46f5bbd3bdd9580dbfacc2
Emilio Gomez Lavin <emiliogomezlavin@gmail.com>
2016-10-18 10:41:58 -0700

    first proper http request generated. Recovers all inks correctly

--- app/models/shirt.rb
+++ app/models/shirt.rb
@@ -1,20 +1,19 @@
 class Shirt < ActiveRecord::Base
 
-	auth_token = '41dc491d-6000-401e-9e3c-368180046554'
+	@auth_token = '41dc491d-6000-401e-9e3c-368180046554'
 
 
 	def self.get_inks ()
-		# category_data = category.split("+")
-		response = HTTParty.get('http://challenge.teespring.com/v1/inks', :headers => { "Authorization" => auth_token})
-		# response["data"].each do |content|
-		# 	url = content["images"]["fixed_height"]["url"]
-		# 	Content.create({
-		# 		title: category_data[0] + " " + category_data[1],
-	 #            url: url,
-	 #            category: "GIF",
-	 #            sentiment: category_data[0]
-		# 		})
-		# end
+		
+		response = HTTParty.get('http://challenge.teespring.com/v1/inks', :headers => { "Auth-Token" => @auth_token})
+		binding.pry
+		puts response
+		
 	end
+
+	# def self.get_questions
+	# 	response = HTTParty.get('http://challenge.teespring.com/v1/inks', :headers => { "Auth-Token" => @auth_token})
+	# end
+
 
 end
